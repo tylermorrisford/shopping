29f4dbd45fd5a74c9ccacf1ea534997a2ab57891
Tyler Ford <tylerford@gmail.com>
2020-03-13 01:24:55 -0600

    Updates

---
+++ precache-manifest.f44ed574ddc970580c175b242306c910.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9617bdac2bedfef97a234218e5f2689c",
+    "url": "/shopping/index.html"
+  },
+  {
+    "revision": "e2a380d28571044a7fff",
+    "url": "/shopping/static/css/2.117ef58c.chunk.css"
+  },
+  {
+    "revision": "17cbae9a012052f565ac",
+    "url": "/shopping/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e2a380d28571044a7fff",
+    "url": "/shopping/static/js/2.ec7de77e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/shopping/static/js/2.ec7de77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17cbae9a012052f565ac",
+    "url": "/shopping/static/js/main.a94cd3ac.chunk.js"
+  },
+  {
+    "revision": "05842cccc404efaebfc8",
+    "url": "/shopping/static/js/runtime-main.24be07d1.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/shopping/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/shopping/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/shopping/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/shopping/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/shopping/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
